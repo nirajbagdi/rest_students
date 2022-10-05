249b3da62e2bf9c0c97d617b01357ed452817aa1
nirajbagdi <bagdiniraj22@gmail.com>
2022-10-05 17:32:51 +0530

    initial commit

---
+++ src/main/java/com/example/rest_students/RestStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestStudentsApplication.class, args);
+	}
+
+}
